2b2fd955e362ab771b6af7db7625f6c8501afd86
Andre-FP <andre.pereira@poli.ufrj.br>
2025-11-30 04:59:45 +0000

    Trained RF_DETR synth

--- RF_DETR/install.sh
+++ RF_DETR/install.sh
@@ -8,19 +8,19 @@ CUDA_VERSION="cu128"
 
 
 
-if conda env list | grep -q "^$MODEL_NAME "; then
-    echo ">>> Ambiente '$MODEL_NAME' já existe. Pulando criação."
-else
-    echo ">>> Criando ambiente conda..."
-    conda create -n "$MODEL_NAME" python="$PYTHON_VERSION" -y
-fi
+#if conda env list | grep -q "^$MODEL_NAME "; then
+#    echo ">>> Ambiente '$MODEL_NAME' já existe. Pulando criação."
+#else
+#    echo ">>> Criando ambiente conda..."
+#    conda create -n "$MODEL_NAME" python="$PYTHON_VERSION" -y
+#fi
 
-echo ">>> Instalando PyTorch..."
-conda run -n $MODEL_NAME pip install torch torchvision \
-    --index-url https://download.pytorch.org/whl/$CUDA_VERSION
+#echo ">>> Instalando PyTorch..."
+#conda run -n $MODEL_NAME pip install torch torchvision \
+#    --index-url https://download.pytorch.org/whl/$CUDA_VERSION
 
 echo ">>> Instalando requirements.txt..."
-conda run -n $MODEL_NAME pip install -r requirements_$MODEL_NAME.txt
+conda run -n $MODEL_NAME pip install -r requirements.txt
 
 echo ">>> Registrando kernel Jupyter..."
 conda run -n $MODEL_NAME python -m ipykernel install --user \
--- RF_DETR/rf_detr.py
+++ RF_DETR/rf_detr.py
@@ -9,15 +9,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device detectado: {device}")
 
 # Caminhos do dataset
-ROOT_DIR = "../SVRDD_COCO"
-OUTPUT_PATH = "./output_rfdetr_final"
-CHECKPOINT = "/workspace/RF_DETR/output_rfdetr_final/checkpoint0034.pth"
+ROOT_DIR = "../../SVRDD_COCO"
+OUTPUT_PATH = "./output_rfdetr_synth"
+EPOCHS = 50
 
-model = RFDETRBase(pretrain_weights=CHECKPOINT)
+model = RFDETRBase()
 
 model.train(
     dataset_dir=ROOT_DIR,
-    epochs=66,
+    epochs=EPOCHS,
     batch_size=8,
     grad_accum_steps=2,
     lr=1e-4,
